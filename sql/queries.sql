821ad91334c8df6909d696e53f7ad9dcc4c11606
Muktha5 <yadav.mrr7@gmail.com>
2026-03-23 22:03:06 +0530

    Create queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,16 @@
+-- Join orders, products, and payments
+
+SELECT
+  o.order_id,
+  o.customer_id,
+  i.product_id,
+  p.product_category_name,
+  pay.payment_type,
+  pay.payment_value
+FROM orders o
+JOIN order_items i
+  ON o.order_id = i.order_id
+JOIN products p
+  ON i.product_id = p.product_id
+JOIN order_payments pay
+  ON o.order_id = pay.order_id;
